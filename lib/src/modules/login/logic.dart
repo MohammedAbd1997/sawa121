b4ec781dea19edcca3eff82d9b77717609ffaf37
pc <d.mohammed2022@gmail.com>
2022-02-27 20:15:26 +0200

    new

--- lib/src/modules/login/logic.dart
+++ lib/src/modules/login/logic.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sawa/src/data/reomte/api_requests.dart';
@@ -11,7 +12,6 @@ import 'package:sawa/src/modules/on_boarding/view.dart';
 import 'package:sawa/src/utils/error_handler/error_handler.dart';
 
 class LoginLogic extends GetxController {
-
   final ApiRequests _apiRequests = Get.find();
   final PrefManger _prefManger = Get.find();
   bool isLoading = false;
@@ -19,6 +19,7 @@ class LoginLogic extends GetxController {
   final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: [
     'email',
   ]);
+  FacebookLogin _facebookLogin = FacebookLogin();
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   void googleSignIn() async {
@@ -34,14 +35,12 @@ class LoginLogic extends GetxController {
       return;
     }
     GoogleSignInAuthentication googleSignInAuthentication =
-    await googleUser.authentication;
+        await googleUser.authentication;
     final AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleSignInAuthentication.idToken
-        , accessToken: googleSignInAuthentication.accessToken
-    );
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
 
-   await _auth.signInWithCredential(credential);
-
+    await _auth.signInWithCredential(credential);
 
     if (googleSignInAuthentication == null) {
       isLoading = false;
@@ -49,9 +48,8 @@ class LoginLogic extends GetxController {
       return;
     }
     var idToken = googleSignInAuthentication.idToken;
-   //   print(idToken);
+    //   print(idToken);
     await socialLogin(idToken, "google");
-
 
     isLoading = false;
     update();
@@ -61,8 +59,9 @@ class LoginLogic extends GetxController {
   void loginByFacebook() async {
     isLoading = true;
     update();
-    final LoginResult result = await FacebookAuth.instance.login();
+    final LoginResult result = await FacebookAuth.instance.login(permissions: ["public_profile","email"]);
     if (result.status == LoginStatus.success) {
+
       AccessToken? _accessToken = result.accessToken;
       log(_accessToken!.token);
       await socialLogin(_accessToken.token, "facebook");
@@ -73,6 +72,7 @@ class LoginLogic extends GetxController {
     isLoading = false;
     update();
   }
+
 
   socialLogin(String? token, String provider) async {
     isLoading = true;
